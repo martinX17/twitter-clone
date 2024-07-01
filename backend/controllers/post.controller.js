ee48936d1893583741d782cc5e881b516bca18ac
martinX17 <yakubmaniyar1709@gmail.com>
2024-07-01 06:04:59 +0530

    10 - Create post updated and payload limit updated

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -3,38 +3,37 @@ import Post from "../models/post.model.js";
 import { v2 as cloudinary } from "cloudinary";
 import Notification from "../models/notification.model.js";
 
-export const createPost = async (req,res) => {
-    try {
-        const {text} = req.body;
-        let {img} = req.body;
-        const userId = req.user._id.toString();
+export const createPost = async (req, res) => {
+	try {
+		const { text } = req.body;
+		let { img } = req.body;
+		const userId = req.user._id.toString();
 
-        const user = await User.findById(userId);
+		const user = await User.findById(userId);
+		if (!user) return res.status(404).json({ message: "User not found" });
 
-        if(!user) return res.status(404).json({message : "User not found"});
+		if (!text && !img) {
+			return res.status(400).json({ error: "Post must have text or image" });
+		}
 
-        if(!text && !img) return res.status(400).json({error : "Post must have a text or image  "});
+		if (img) {
+			const uploadedResponse = await cloudinary.uploader.upload(img);
+			img = uploadedResponse.secure_url;
+		}
 
-        if(img){
-            const uploadedResponse = await cloudinary.uploader.upload(img);
-            img = uploadedResponse.secure_url;
-        }
+		const newPost = new Post({
+			user: userId,
+			text,
+			img,
+		});
 
-        const newPost = new Post({
-            user : userId,
-            text,
-            img,
-
-        })
-
-        await newPost.save();
-        res.status(201).json(newPost);
-
-    } catch (error) {
-        res.status(500).json({error : "Internal server error"});
-        console.log("Error in createPost controller",error)
-    }
-}
+		await newPost.save();
+		res.status(201).json(newPost);
+	} catch (error) {
+		res.status(500).json({ error: "Internal server error" });
+		console.log("Error in createPost controller: ", error);
+	}
+};
 
 export const deletePost = async (req,res) => {
     try {
